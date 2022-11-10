b8ca5657bab0608d7e419cfe9c94013de63cc423
substanc3-dev <jano4varga@gmail.com>
2022-11-10 18:27:23 +0100

    Fix random crashes on attempted achievement

--- src/main/java/dev/substanc3/ic2fix/IC2FixMod.java
+++ src/main/java/dev/substanc3/ic2fix/IC2FixMod.java
@@ -1,7 +1,10 @@
 package dev.substanc3.ic2fix;
 
+import ic2.core.IC2;
+import ic2.core.IC2Achievements;
 import ic2.core.ref.Ic2Fluids;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.entity.BlockEntityType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +12,7 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-public class IC2FixMod implements ClientModInitializer {
+public class IC2FixMod implements ModInitializer, ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("ic2fix");
 
 	public static final List<BlockEntityType> IC2_BLOCK_ENTITIES = new ArrayList<>();
@@ -18,5 +21,10 @@ public class IC2FixMod implements ClientModInitializer {
 	public void onInitializeClient() {
 		LOGGER.info("Client init!");
 		Ic2Fluids.init();
+	}
+
+	@Override
+	public void onInitialize() {
+		IC2.achievements = new IC2Achievements();
 	}
 }
